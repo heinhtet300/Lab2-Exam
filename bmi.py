cd5dc5bfa13f8c3572ee67e0cc518487480708b3
HeinHtet <HEINZ.25@ichat.sp.edu.sg>
2026-05-26 21:38:26 +0800

    new

--- bmi.py
+++ bmi.py
@@ -2,12 +2,13 @@ def calculate_bmi(height,weight):
     print("Height = "+ str(height))
     print("Weight = "+ str(weight))
     bmi = weight / (height * weight)
-    return bmi
+    if bmi > 25 :
+        value = 1
+    elif weight >= 18.5 and weight <= 25:
+        value = 0
+    elif weight < 18.5:
+        value = -1
+    return value
     
 weight = calculate_bmi(weight=57,height=1.73)
-if weight > 25 :
-    print("Over Weight")
-elif weight >= 18.5 and weight <= 25:
-    print("Normal Weight")
-elif weight < 18.5:
-    print("Under Weight")
+
